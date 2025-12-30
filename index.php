60740d112482ad7371cebdd4a08ba3375b452233
Md Tareq Miah <mdtareqmia25@gmail.com>
2025-12-30 20:33:15 +0600

    php Data type

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <title>PHP syntax</title>
     <style>
         body{font-family: <?php echo $fonts; ?>;}
-        .phpcoding{ width:900px; margin: 0 auto; background: <?php echo "#dfd" ?> ; padding: 20px; min-height: 600px;}
+        .phpcoding{ width:1000px; margin: 0 auto; background: <?php echo "#dfd" ?> ; padding: 20px; min-height: 600px;}
         .headeroption, .footeroption {background: <?php echo $bgcolor ?>; color: <?php echo $fontcolor ?>; text-align: center; padding: 20px;}
         .headeroption h2, .footeroption h2{margin: 0;}
         .maincontent {min-height: 500px; padding: 20px;}
@@ -28,6 +28,8 @@
         </section>
 
         <section class="maincontent">
+            
+
             <?php
                 $a=5;
                 $b=7;
@@ -39,10 +41,97 @@
                 print "php is nice from print<br>";
                 $x= print "Tareq";
                 var_dump($x);
-
-
-
             ?>
+            
+            <?php
+                echo "PHP Data types:"
+            ?>
+            1.string 
+            2.Integer
+            3.Float
+            4.Boolean
+            5.Array
+            6.Object
+            7.NULL
+            8.Resources
+            <br>
+            <hr>
+            1.string
+            <hr>
+            <?php
+                $x='this is string data types';
+                echo $x;
+            ?>
+            <hr>
+            2,3.Integer
+            <hr>
+            <?php
+                $a="This is integer data type.";
+                echo $a, PHP_EOL;
+                $b=57;
+                echo $b,PHP_EOL;
+                $c=56.5;
+                var_dump($a,$b,$c);
+            ?>
+            <hr>
+            4.Boolean
+            <hr>
+            <?php
+                $x=true;
+                var_dump($x);
+                $x=false;
+                var_dump($x);
+            ?>
+            <hr>
+            5.Array
+            <hr>
+            <?php
+            //array 0 indexing 
+                $x=["Tareq", "Asif", "Fuad"];
+                foreach($x as $y){
+                    echo $y;
+                    echo "<br>";
+                }
+                var_dump($x);
+            ?>
+            <hr>
+            6.Object
+            <hr>
+            <?php
+                //data store information store 
+                class student{
+                    public $roll=36;
+                    //inner class write function those called method
+                    function department(){
+                        return "This is from department function Physics";
+                        //return $roll;
+                    }
+                    function details(){
+                        echo $this->department();
+                        
+                    }
+                }
+                $stu = new student();
+                $stu->details();
+            ?>
+            <hr>
+            7.Null 
+            <hr>
+            <?php
+                //null is a special datatype
+                /// this is not zero not empty
+                $x=NULL;
+                var_dump($x);
+            ?>
+            <hr>
+            8. Resources
+            <hr>
+            <?php
+                //this is store function references
+                //called database
+                
+            ?>
+
 
         </section>
     
